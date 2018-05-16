21e9887999cb7af66c628cc98538f4006dae9d67
Kaijubear <k.thomasbooth@gmail.com>
2018-05-16 21:17:23 +1200

    fixed access bug

---
+++ TestBot/BotApplication1/Dialogs/RootDialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotApplication1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        //Wait for a message from the user
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // Return our reply to the user
+            await context.PostAsync($"You sent {activity.Text}");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
